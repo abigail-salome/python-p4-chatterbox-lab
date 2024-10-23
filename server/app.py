563279a1f5b2f461fecdfc3018d03aa3db214d6a
Abigail Salome <salomeabigail9@gmail.com>
2024-10-24 00:21:18 +0300

    writes code on python-p4-chatterbox-lab

--- server/app.py
+++ server/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
-from models import db, Message
+from models import (
+    db,
+    Message,
+)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +16,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET"])
 def messages():
-    return ''
+    """Returns an array of all messages as JSON, ordered by created_at in ascending order."""
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
 
-if __name__ == '__main__':
+@app.route("/messages", methods=["POST"])
+def create_message():
+    """Creates a new message with a body and username from params, returning the new message as JSON."""
+    data = request.get_json()
+    new_message = Message(body=data["body"], username=data["username"])
+    db.session.add(new_message)
+    db.session.commit()
+    return make_response(jsonify(new_message.to_dict()), 201)
+
+
+@app.route("/messages/<int:id>", methods=["PATCH"])
+def update_message(id):
+    """Updates the body of the message using params, returning the updated message as JSON."""
+    data = request.get_json()
+    message = Message.query.get_or_404(id)
+    message.body = data["body"]
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+
+@app.route("/messages/<int:id>", methods=["DELETE"])
+def delete_message(id):
+    """Deletes the message from the database."""
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return make_response("", 204)
+
+
+if __name__ == "__main__":
     app.run(port=5555)
